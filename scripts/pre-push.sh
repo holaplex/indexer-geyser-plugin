d019bf5c46ad02e63fc8345e03be2cfd1d1e47d3
raykast <me@ryan-s.net>
2022-06-15 14:19:15 -0700

    Initial commit.

---
+++ scripts/pre-push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")/.."
+
+lib_flags=(--workspace --lib --all-features)
+build_flags=("${lib_flags[@]}")
+build_flags+=(--bins)
+
+[[ -z "$CARGO" ]] && CARGO=cargo
+
+"$CARGO" fmt --all --check
+"$CARGO" clippy "${build_flags[@]}" --no-deps
+"$CARGO" doc "${lib_flags[@]}" --no-deps
+"$CARGO" build "${build_flags[@]}"
+"$CARGO" test "${build_flags[@]}"
